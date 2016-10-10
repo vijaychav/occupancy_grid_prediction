306f69cb52d8cc30909631a3af90c8942a14c68e
vijaychav <vijaynight123@gmail.com>
2016-10-09 17:00:48 -0700

    adding essential files

---
+++ README.md
@@ -0,0 +1,48 @@
+# Occupancy Grid Estimation
+
+
+##### Instructions for set up
+* You will need Tensorflow and GPU support with CUDA to train or test the deep learning network. The complete instructions to install and set up the environment are at this [link](https://www.tensorflow.org/versions/r0.11/get_started/os_setup.html)
+* Download the gip repo and mention the location of the hd5y file in the python file main.py. Save the file
+
+##### Describe the approach you took and why you chose it
+* Between supervised and unsupervised learning, since we had a large dataset, the context of the sequence of any 20 frames could be learn't thoroughly using supervised learning. In techniques of supervised learning, neural networks are found to be the most powerful tool in time sequence prediction in capturing the temporal relation between the sequential frames. I decided to build a deep learning model on tensorflow which made use of LSTMs. 
+* The **rms error** during testing dropped to as low as **10**
+* I divided the given set of 1,000,000 images into a training set of 700,000 images and a testing set of 100,000 images. Each sample used to train the network is a sequence of 20 frames with each frame in the sequence being a *100x100* image. The target against which each sequence is trained is the ground truth image at the 21st (or the next) index.
+
+##### Running the model
+* Make sure the hd5y file path is mentioned in the file main.py
+* To run use:
+  ```python
+  sudo python main.py <input index>
+  ```
+* Make sure **input index is between 700,000 and 1,000,000** since the input index should point to an index in the test set and not the training set 
+* The network uses a already trained model in the folder [./trainedModels](./trainedModels)
+* The output will be the ground truth image and the image predicted using the previous 20 frames of occupancy grid. The system also prints out the accuracy and the rms error.
+
+##### Advantages and disadvantages
+* Advantages:
+	* Since we are aiming at capturing the temporal relation between 20 frames, LSTMs are proven to do a very good job at that when compared to other model like Hidden Markov models etc
+* Disadvantages:
+	* Computationally intensive 	
+	
+##### Relevant materials, if used (books, papers, blogs, ...)
+* Used my previous experience on time sequence prediction using neural networks.
+
+##### Breakdown of time spent
+* Total 22 hours:
+  * 6 hours on trying methods of bayesian estimation and validating any changes of usage of unsupervised learning.
+  * 2 hours on setting up the environment for deep learning
+  * 12 hours for building and analysing the results of the deep leanrning model
+  * 2 hours for setting up readme and pushing code to git
+
+##### Future directions you would take if you had more time
+* What would you do with a few days? with a few weeks?
+	* Still a lot of optimization can be done on the rms error. I would spend my time on that
+    
+* What if your algorithm needed to run in real time (20hz)?
+	* Since the model is already trained, the model can be used in real time. You just have to feed data sequentially into the system.
+* Our simulated sensor data is very clean, how would you deal with noisy real world sensor data.
+	* Noise such as salt and pepper noise can be removed using techniques such as median filtering. Extermeties can be removed using PCA decomposition. Given a large enough dataset, the deep learning model is robust enough to alleviate noise. 
+* Can your approach infer future states and if so how effective
+	* Yes, the predicted frame can be fed back into the network to generate the next frame and so on. Therefore the model can be used to generate sequences given an initial sequence. I haven't implemented and tested this for our scenario but reseach shows that LSTMs can generate meaningful word sentences and phrases when trained well.
---
+++ main.py
@@ -0,0 +1,221 @@
+"""
+import os
+def clear_all():
+    '''Clears all the variables from the workspace of the spyder application.'''
+    gl = globals().copy()
+    for var in gl:
+        if var[0] == '_': continue
+        if 'func' in str(globals()[var]): continue
+        if 'module' in str(globals()[var]): continue
+        del globals()[var]
+clear_all()
+"""
+
+import h5py
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+#from scipy import ndimage
+import tensorflow as tf
+#from scipy import signal
+
+historyRange = 20
+modelName = "./trainedModel.ckpt"
+loadModel = 0
+
+#np.set_printoptions(threshold=np.inf)
+
+
+with h5py.File('/media/vijay/Windows/InvincE/GoogleDrive/Full time app/Dispatch/data.h5','r') as hf:
+    print('List of arrays in this file: \n', hf.keys())
+    occupancy = hf.get('occupancy')
+    groundtruth = hf.get('ground_truth')
+    print occupancy.shape
+
+    #occupancyData = occupancy[:,:,inputIndex-historyRange:inputIndex]
+    train_test_split = 0.7
+    num_images = occupancy.shape[2]
+    
+    def quantize(x,case):
+        if case==0: #occupancy data
+            x[np.where(x==0)] = -1 
+            x[np.where(x==127)] = 0
+            x[np.where(x==255)] = 1
+        elif case ==1: #ground truth data
+            x[np.where(x==0)] = 0
+            x[np.where(x==255)] = 1
+        return x
+    
+    def getDataTrain():
+        step =0
+        while step+historyRange < int(train_test_split*num_images):
+            X_tr = np.array(occupancy[:,:,step:step+historyRange]).transpose([2,1,0]).reshape([historyRange,-1]).astype(int)
+            #print X_tr.shape
+            Y_tr = np.array(groundtruth[:,:,step+historyRange]).flatten().astype(int)
+            #print Y_tr.shape
+            X_tr = quantize(X_tr,0)
+            Y_tr = quantize(Y_tr,1)
+            yield X_tr, Y_tr
+            step+=1
+            
+    def getDataTest(index=None):
+        if index:
+            step = index-historyRange
+        else:
+            step = int(train_test_split*num_images)
+        while step+historyRange< num_images:
+            X_te = np.array(occupancy[:,:,step:step+historyRange]).transpose([2,1,0]).reshape([historyRange,-1]).astype(int)
+            #print X_te.shape
+            Y_te = np.array(groundtruth[:,:,step+historyRange]).flatten().astype(int)
+            #print Y_te.shape
+            X_te = quantize(X_te,0)
+            Y_te = quantize(Y_te,1)
+            yield X_te, Y_te
+            step+=1
+            
+            
+    
+    print "Number of images:", num_images
+    trainData = getDataTrain()
+    testData = getDataTest()
+
+    #print type(next(data))
+    #print next(trainData)[0].shape
+
+    num_train = int(train_test_split*num_images)
+
+    learning_rate = 0.01
+    training_iters = 900*num_train
+    num_batch = 1
+    max_grad_norm = 10
+    lossratio = 5.0
+    num_layers = 1
+    num_hidden = 128
+    
+    
+    with tf.device('/gpu:0'):
+        x = tf.placeholder(tf.float32,[None,20,10000])
+        y = tf.placeholder(tf.float32,[None,10000])
+        weight = tf.Variable(tf.random_normal([num_hidden, int(y.get_shape()[1])]))
+        bias = tf.Variable(tf.random_normal([int(y.get_shape()[1])]))
+        cell = tf.nn.rnn_cell.LSTMCell(num_hidden,state_is_tuple=True)
+        istate = cell.zero_state(1,tf.float32)
+        val, state = tf.nn.dynamic_rnn(cell, x, dtype=tf.float32, initial_state= istate)
+    
+        val = tf.transpose(val, [1,0,2])
+        last = tf.gather(val, int(val.get_shape()[0])-1)
+        print "shape of last:", last.get_shape()
+        #bias = tf.Variable(tf.constant(0.1, shape=[y.get_shape()[1]]))
+        logit = tf.nn.xw_plus_b(last, weight,bias)
+        #logit_sig = tf.sign(tf.sub(tf.sigmoid(logit),0.5))
+        logit_sig = tf.round(tf.sigmoid(logit))
+        
+        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logit,y))
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars), max_grad_norm)
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate) # Adam Optimizer
+        train_op = optimizer.apply_gradients(zip(grads, tvars))
+        correct = tf.equal(y,logit_sig)
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+        #print "y:", y
+        #print "logit_sig:", logit_sig
+        rmserr = tf.sqrt(tf.reduce_sum(tf.square(tf.sub(y,logit_sig))))
+       
+    
+    
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    init_op = tf.initialize_all_variables()
+    
+    if not loadModel:  #training model from stratch
+        saver = tf.train.Saver()
+    
+        sess.run(init_op)
+        step=0
+        #while step+historyRange< num_images:
+        while step+historyRange< 2000:
+            train, trainTarget = next(trainData)
+            train = [train]; trainTarget = [trainTarget]
+            startTime = time.clock()
+            sess.run(train_op, feed_dict={x:np.array(train), y:np.array(trainTarget)})
+            #output = sess.run(logit_sig, feed_dict={x:np.array(train), y:np.array(trainTarget)})
+            acc = sess.run(accuracy, feed_dict={x:np.array(train), y:np.array(trainTarget)})
+            rmserror = sess.run(rmserr, feed_dict={x:np.array(train), y:np.array(trainTarget)})
+            los = sess.run(loss, feed_dict={x:np.array(train), y:np.array(trainTarget)})
+            #print "Shape of output:",output.shape
+            print "Train Accuracy: %.3f, loss: %.3f, train rmserr: %.3f, step: %d, time taken: %.5f sec" %(acc,los,rmserror,step, time.clock() - startTime)
+
+            #print "loss:", loss
+            step+=1
+            if step%10==0:
+                steptest = 0
+                testacc = []
+                while steptest<1:
+                    test, testTarget = next(testData)
+                    test= [test]; testTarget = [testTarget]
+                    testaccperstep = sess.run(accuracy, feed_dict={x:np.array(test), y:np.array(testTarget)})
+                    rmserror = sess.run(rmserr, feed_dict={x:np.array(test), y:np.array(testTarget)})
+                    testacc.append(testaccperstep)
+                    steptest+=1
+                print "Test Accuracy, %.3f, Test Rms err: %.3f" %(np.mean(np.array(testacc)),rmserror)
+        
+        save_path = saver.save(sess, modelName)
+        print "Model saved at ",save_path 
+    
+    else:
+        inputIndex = sys.argv[1]
+        assert inputIndex > int(train_test_split*num_images) and inputIndex<occupancy.shape[-1], "Input index should be between 700000 and the max number of frames"
+
+        sess.run(init_op)
+        testData = getDataTest(inputIndex)
+        saver = tf.train.Saver()
+        saver.restore(sess, "./trainedModels/"+modelName)
+        print "Using saved model......"
+        test, testTarget = next(testData)
+        #print test
+        plt.figure(3)
+        plt.imshow(test[-1].reshape([100,100]))
+        plt.colorbar(orientation='vertical')
+        plt.title("Occupancy")
+        print "testTarget:", testTarget
+        test= [test]; testTarget = [testTarget]
+        testTargetShow = 255*(testTarget[0].reshape([100,100]))        
+        testacc = sess.run(accuracy, feed_dict={x:np.array(test), y:np.array(testTarget)})
+        output = sess.run(logit_sig, feed_dict={x:np.array(test), y:np.array(testTarget)})
+        rmserror = sess.run(rmserr, feed_dict={x:np.array(test), y:np.array(testTarget)})
+        print "Test Accuracy:", testacc
+        print "Rms Error:", rmserror
+        
+        outputShow = 255*(output[0].reshape([100,100]))
+        #print "Error distance = ", np.linalg.norm(testTargetShow.flatten()-outputShow.flatten())
+        plt.figure(1)
+        plt.imshow(testTargetShow)
+        plt.colorbar(orientation='vertical')
+        plt.title("Ground Truth")
+        plt.figure(2)
+        plt.imshow(outputShow)
+        plt.colorbar(orientation='vertical')
+        plt.title("Prediction")
+        plt.show()
+    
+    
+    
+
+                
+
+    
+    
+    
+    sess.close()
+    hf.close()
+    
+    
+    
+    
+ 
+ 
+    
+    
+    
+    
+    
